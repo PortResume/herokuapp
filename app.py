b22330c54752bf69f1909d94275dd2a01f4376ca
Skylar Barrowman <45975571+PortResume@users.noreply.github.com>
2019-04-02 21:27:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,6 @@ from bottle import route, run
 def index(name='World'):
     return '<b>Hello %s!</b>' % name
 
-@route('/view/')
-def index():
-    """Home page"""
-
-    info = {'title': 'Welcome Home!',
-            'content': 'Hello World'
-            }
-
-    return template('simple.tpl', info)
 
 
 if __name__ == '__main__':
